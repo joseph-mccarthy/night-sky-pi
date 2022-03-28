b34ae5f67c6c51265301dc748ce096f4ebf74719
Joseph McCarthy <joseph-mccarthy@users.noreply.github.com>
2022-03-28 21:01:17 +0100

    dates

--- src/sun_functions.py
+++ src/sun_functions.py
@@ -4,6 +4,6 @@ from datetime import datetime
 def get_rise_and_set(date:datetime,coorindates:tuple()) -> tuple():
     latitude, longitude = coorindates
     sun = Sun(latitude, longitude)
-    abd_sr = sun.get_sunrise_time(date).replace(tzinfo=None)
-    abd_ss = sun.get_sunset_time(date).replace(tzinfo=None)
-    return (abd_sr,abd_ss)
+    sr = sun.get_sunrise_time(date).replace(tzinfo=None)
+    ss = sun.get_sunset_time(date).replace(tzinfo=None)
+    return (sr,ss)
