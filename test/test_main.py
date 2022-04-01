8ac414bc8bd57ba37b647d2f1413e1526d32f91d
Joseph McCarthy <joseph-mccarthy@users.noreply.github.com>
2022-04-01 14:23:37 +0100

    remove brackets

--- test/test_main.py
+++ test/test_main.py
@@ -8,7 +8,7 @@ from src.data import SunData
 class TestMain(TestCase):
 
     @patch('src.main.time._get_now', return_value=datetime(2022, 2, 1, 15, 30))
-    @patch('src.main.sun.get_rise_and_set', return_value=(SunData(datetime(2022, 2, 1, 5, 30), datetime(2022, 2, 1, 18, 30))))
+    @patch('src.main.sun.get_rise_and_set', return_value=SunData(datetime(2022, 2, 1, 5, 30), datetime(2022, 2, 1, 18, 30)))
     @patch('src.main.Main')
     @patch('src.main.capture')
     def test_time_to_process(self, capture, mock_time, mock_sun, main_mock):
